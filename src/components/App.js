fe325bff3c106d80afff61731c3dad3f950faebe
BaD_AppLE_ <elucidator.engineer@yahoo.com>
2017-12-26 04:56:57 +0100

    started adding react components, have some bugs, but so far so good

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,25 @@ import "bootswatch/lumen/bootstrap.css";
 import { Navbar,NavDropdown, MenuItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import '../styles/App.css';
 
-
+class WeatherDisplay extends Component {
+    render() {
+    const weatherData = this.props.weather;
+    if (!weatherData) return <div>Loading...</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°C</p>
+        <p>High: {weatherData.main.temp_max}°C</p>
+        <p>Low: {weatherData.main.temp_min}°C</p>
+        <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+      </div>
+    );  }
+}
 class App extends Component {
     componentDidMount() {
         this.props.fetchData('http://api.openweathermap.org/data/2.5/weather?q=New York&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial');
@@ -15,11 +33,44 @@ class App extends Component {
 
     render() {
        return (
-        <div>
-            <p>58585</p>
-            <p>{ this.props.weather.name }</p>
-            <p>{ this.props.activePlace }</p>
-            <p>{ this.props.cities[0].name }</p>
+        <div className="App">
+        {/*<div>
+                    <p>{ this.props.weather.name }</p>
+                    <p>{ this.props.activePlace }</p>
+                    <p>{ this.props.cities[0].name }</p>
+                </div>*/}
+            <div>
+                <Navbar>
+                  <Navbar.Header>
+                    <Navbar.Brand>
+                      Weather In Serbia
+                    </Navbar.Brand>
+                  </Navbar.Header>
+                </Navbar>
+                <Grid>
+                    <Row>
+                        <Col md={3} sm={3} lg={3} >
+                          <Nav
+                            bsStyle="pills"
+                            stacked
+                            activeKey={this.props.activePlace}
+                            onSelect={index => {
+                              setActivePlace(index)
+                            }}>
+                            <NavDropdown eventKey="4" title="Select a city" id="nav-dropdown">
+                            {this.props.cities.map((city, index) => (
+                              <MenuItem key={index} eventKey={index}>{city.name}</MenuItem>
+                            ))}
+                            </NavDropdown>
+                            
+                          </Nav>
+                        </Col>
+                        <Col md={9} sm={9} lg={9}>
+                            <WeatherDisplay key={0} cityName={this.props.weather.name} />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
         </div>
         )
     };
